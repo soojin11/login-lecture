ef64303c7be735894bc5502652b9867ae7dd9f75
soojin <soojin@wgslabs.com>
2022-03-16 19:18:08 +0900

    fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,11 +7,25 @@ const id = document.querySelector("#id"),
 loginBtn.addEventListener("click", login);
 
 function login(){
-    console.log(id.value);
-
+    //전달 할 데이터
     const req = {
         id: id.value,
         psword: psword.value,
     }
-    console.log(req);
+
+    // fetch로 전달
+    // 경로 지정
+    // stringify하는 이유 : 문자열로 전달하기 위해
+    // body를 통해 전달하는 경우 method : "POST"
+    // 내가 전달하는 형식이 json이라는 것을 알려주기 위해 headers에 작성
+    // post라는 method로 받을 수 있는 경로가 있어야 함(index.js)
+    fetch("/login", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)
+    })
+
+
 }
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,7 +7,6 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.home);
-
 router.get("/login", ctrl.login);
 
 //외부로 내보내기
