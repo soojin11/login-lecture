234b3866f417ea0607151ab91a6c913dc2e818f0
soojin <soojin@wgslabs.com>
2022-03-17 18:07:08 +0900

    23 회원가입화면

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 //외부로 내보내기
